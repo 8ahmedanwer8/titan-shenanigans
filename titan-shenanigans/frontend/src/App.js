cdd34b21a646656ecc89b6432828097540401aa6
8ahmedanwer8 <sahmedanwer@gmail.com>
2022-07-26 21:34:46 -0400

    got chakra-ui sorted out, creating browser routes for the pages

---
+++ titan-shenanigans/frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Button } from "@chakra-ui/button";
+function App() {
+  return (
+    <div className="App">
+      <Button colorScheme="blue">Button</Button>{" "}
+    </div>
+  );
+}
+
+export default App;
